ee0ed50ce75b8c93b509a3f5c1535052a0e2c13c
Joseph Duffy <josephduffy@me.com>
2017-12-17 18:19:06 +0000

    Adds validation to `SurveyResult`
    Replaces `SurveyResult.anonymous` with `SurveyResult.showName`

--- src/interfaces/SurveyResult.ts
+++ src/interfaces/SurveyResult.ts
@@ -1,12 +1,20 @@
-import { IsEmail } from 'class-validator';
+import { ArrayMinSize, IsBoolean, IsEmail, MinLength } from 'class-validator';
 
 namespace SurveyResult {
   export class Body {
+
+    @ArrayMinSize(2)
     public readonly answers: Array<{ [inputId: string]: any }>;
+
+    @MinLength(2)
     public readonly name: string;
-    public readonly anonymous: boolean;
+
+    @IsBoolean()
+    public readonly showName: boolean;
+
     @IsEmail()
     public readonly email: string;
+
   }
 
   export class Full extends Body {
